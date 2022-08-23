5cb5a04e1d01789a7cb7a0a51784120925866554
ctang415 <carrietang117@gmail.com>
2022-08-23 19:51:32 -0400

    Made mouse over square change colors

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
 const container = document.querySelector('.container');
 
 function createSquare() {
-    for (let i = 0; i < 256; i+= 1) {
-    const div = document.createElement('div');
-    div.classList.add('div');
-    container.appendChild(div);
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('div');
+        div.addEventListener('mouseover', function() {
+            div.style.backgroundColor = 'blue';
+        })
+        div.addEventListener('mouseout', function() {
+            div.style.backgroundColor = 'white';
+        })
+        container.appendChild(div);
     }
 };
 createSquare();
+
+
