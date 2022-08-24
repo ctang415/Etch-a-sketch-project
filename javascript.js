a3848a9aadccb695d9645c9dddf0457b2e81055f
ctang415 <carrietang117@gmail.com>
2022-08-23 23:35:58 -0400

    Changed square color change method to hover

--- javascript.js
+++ javascript.js
@@ -4,15 +4,11 @@ function createSquare() {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.add('div');
-        div.addEventListener('mouseover', function() {
-            div.style.backgroundColor = 'blue';
-        })
-        div.addEventListener('mouseout', function() {
-            div.style.backgroundColor = 'white';
-        })
         container.appendChild(div);
     }
 };
 createSquare();
+
+
 
 
