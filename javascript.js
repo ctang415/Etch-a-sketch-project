732dfb5a5f0534a53b41c6aa5e92f044dcf9ac87
ctang415 <carrietang117@gmail.com>
2022-08-24 01:17:12 -0400

    Created function to get random colors

--- javascript.js
+++ javascript.js
@@ -4,6 +4,9 @@ function createSquare() {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.add('div');
+        div.addEventListener('mouseover', function() {
+            div.style.backgroundColor = getRandomColor();
+        })
         container.appendChild(div);
     }
 };
@@ -11,4 +14,9 @@ createSquare();
 
 
 
-
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
